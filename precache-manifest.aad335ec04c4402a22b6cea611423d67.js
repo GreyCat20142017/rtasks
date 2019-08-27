44b634d793f92feddd58695b917f9c020f2fe482
GreyCat20142017 <nrz3siaatg81@mail.ru>
2019-08-27 22:23:13 +0300

    Updates

---
+++ precache-manifest.aad335ec04c4402a22b6cea611423d67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0691f0b4a5fd83d60cf21f5bde35c2a",
+    "url": "/rtasks/index.html"
+  },
+  {
+    "revision": "ea2832a3621a6785ccf3",
+    "url": "/rtasks/static/css/main.ce83845f.chunk.css"
+  },
+  {
+    "revision": "028f82e5a81ce99735f8",
+    "url": "/rtasks/static/js/2.c558fd2b.chunk.js"
+  },
+  {
+    "revision": "ea2832a3621a6785ccf3",
+    "url": "/rtasks/static/js/main.e1dec422.chunk.js"
+  },
+  {
+    "revision": "099469e29ee5c3b6edc1",
+    "url": "/rtasks/static/js/runtime~main.ae785f54.js"
+  },
+  {
+    "revision": "0d9b647f213111d2adf85345b648ef7c",
+    "url": "/rtasks/static/media/logo.0d9b647f.svg"
+  }
+]);
